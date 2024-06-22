6dae1ccc1144c27cb0a2b55a033e290db17b6eb8
Titov Sergey <sergeyntitov@gmail.com>
2024-06-22 12:22:36 +0300

    added tasks component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,5 @@
-import { Component, computed, Input, input, Output, EventEmitter, signal, output } from '@angular/core'
-import { DUMMY_USERS } from '../../shared/data/mock-data'
+import { Component, EventEmitter, Input, Output } from '@angular/core'
 import { User } from '../../shared/types/user'
-
-// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -10,13 +7,10 @@ import { User } from '../../shared/types/user'
   styleUrl: './user.component.css',
 })
 
-
 export class UserComponent {
   @Input({ required: true }) user!: User
   @Output() select = new EventEmitter<User>()
-
-  // select = output<User>()
-
+  
   onClick() {
     this.select.emit(this.user)
   }
